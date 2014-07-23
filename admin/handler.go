763d51cf7c1c91ba8691746cb4674e44d1dff596
Nattapong Tonprasert <luciferultram@gmail.com>
2014-07-24 03:05:32 +0700

    move public file and handle

---
+++ admin/handler.go
@@ -0,0 +1,80 @@
+package admin
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/go-martini/martini"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/martini-contrib/render"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+type EnryptResult struct {
+	ShowId  int
+	ListId  int
+	VideoId string
+}
+
+func EncryptHandler(r render.Render) {
+	var results []string
+	emptymap := map[string]interface{}{
+		"showid":  "",
+		"message": "",
+		"results": results,
+	}
+	r.HTML(200, "admin/encrypt", emptymap)
+}
+
+func EncryptUpdateHandler(db *sql.DB, params martini.Params, req *http.Request, r render.Render) {
+	idType := req.FormValue("idType")
+	id, _ := strconv.Atoi(req.FormValue("id"))
+
+	var results []*EnryptResult
+
+	if idType == "" || id == 0 {
+		emptymap := map[string]interface{}{
+			"showid":  "",
+			"message": "*Fill the form",
+			"results": results,
+		}
+		r.HTML(200, "admin/encrypt", emptymap)
+		return
+	}
+
+	var rows *sql.Rows
+
+	switch idType {
+	case "showid":
+		rows, _ = db.Query("SELECT program_id, programlist_id, programlist_youtube FROM tv_programlist WHERE program_id = ? ORDER BY programlist_id DESC", id)
+	case "listid":
+		rows, _ = db.Query("SELECT program_id, programlist_id, programlist_youtube FROM tv_programlist WHERE programlist_id = ? ORDER BY programlist_id DESC", id)
+	}
+
+	defer rows.Close()
+	for rows.Next() {
+		var (
+			showId  int
+			listId  int
+			videoId string
+		)
+		if err := rows.Scan(&showId, &listId, &videoId); err != nil {
+			log.Fatal(err)
+		}
+		updateVideoEncrypt(db, listId, videoId)
+		results = append(results, &EnryptResult{showId, listId, videoId})
+	}
+
+	if err := rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	newmap := map[string]interface{}{
+		"showid":  id,
+		"message": fmt.Sprintf("Update Encrypt %s %d", idType, id),
+		"results": results,
+	}
+
+	r.HTML(200, "admin/encrypt", newmap)
+}
---
+++ api2/handler.go
@@ -0,0 +1,203 @@
+package api2
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"github.com/dropbox/godropbox/memcache"
+	"github.com/go-martini/martini"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+const cacheExpireTime = uint32(600)
+
+func setCached(client memcache.ClientShard, key string, value []byte) {
+	item := memcache.Item{
+		Key:        key,
+		Value:      value,
+		Flags:      uint32(123),
+		Expiration: cacheExpireTime,
+	}
+	resp := client.Set(&item)
+	if resp.Status() != 0 {
+		log.Println(resp.Error())
+	}
+}
+
+func AdvertiseListHandler(db *sql.DB, client memcache.ClientShard, params martini.Params, req *http.Request) (int, string) {
+	device := req.URL.Query().Get("device")
+	key := fmt.Sprintf("Api2/AdvertiseListHandler/%s", device)
+
+	res := client.Get(key)
+	if res.Status() == 0 {
+		return http.StatusOK, string(res.Value())
+	}
+
+	advertises := GetAdvertise(db, device)
+	result := &Advertises{advertises}
+	b, err := json.Marshal(result)
+	if err != nil {
+		log.Println(err)
+		return http.StatusInternalServerError, "Oops!!!"
+	}
+
+	setCached(client, key, b)
+	return http.StatusOK, string(b)
+}
+
+func SectionListHandler(db *sql.DB, client memcache.ClientShard, params martini.Params) (int, string) {
+	key := "Api2/SectionListHandler"
+
+	res := client.Get(key)
+	if res.Status() == 0 {
+		return http.StatusOK, string(res.Value())
+	}
+
+	categories := GetCategory(db)
+	channels := GetChannel(db)
+	radios := GetRadio(db)
+	result := &Section{categories, channels, radios}
+	b, err := json.Marshal(result)
+	if err != nil {
+		log.Println(err)
+		return http.StatusInternalServerError, "Oops!!!"
+	}
+
+	setCached(client, key, b)
+	return http.StatusOK, string(b)
+}
+
+func CategoryListHandler(db *sql.DB, client memcache.ClientShard, params martini.Params) (int, string) {
+	key := "Api2/CategoryListHandler"
+
+	res := client.Get(key)
+	if res.Status() == 0 {
+		return http.StatusOK, string(res.Value())
+	}
+
+	categories := GetCategory(db)
+	result := &Categories{categories}
+	b, err := json.Marshal(result)
+	if err != nil {
+		log.Println(err)
+		return http.StatusInternalServerError, "Oops!!!"
+	}
+
+	setCached(client, key, b)
+	return http.StatusOK, string(b)
+}
+
+func CategoryShowHandler(db *sql.DB, client memcache.ClientShard, params martini.Params) (int, string) {
+	id := params["id"]
+	start, _ := strconv.Atoi(params["start"])
+
+	key := fmt.Sprintf("Api2/CategoryShowHandler/%s/%d", id, start)
+
+	res := client.Get(key)
+	if res.Status() == 0 {
+		return http.StatusOK, string(res.Value())
+	}
+
+	shows := GetCategoryShow(db, id, start)
+	result := &Shows{shows}
+	b, err := json.Marshal(result)
+	if err != nil {
+		log.Println(err)
+		return http.StatusInternalServerError, "Oops!!!"
+	}
+
+	setCached(client, key, b)
+	return http.StatusOK, string(b)
+}
+
+func ChannelListHandler(db *sql.DB, client memcache.ClientShard, params martini.Params) (int, string) {
+	key := "Api2/ChannelListHandler"
+
+	res := client.Get(key)
+	if res.Status() == 0 {
+		return http.StatusOK, string(res.Value())
+	}
+
+	channels := GetChannel(db)
+	result := &Channels{channels}
+	b, err := json.Marshal(result)
+	if err != nil {
+		log.Println(err)
+		return http.StatusInternalServerError, "Oops!!!"
+	}
+
+	setCached(client, key, b)
+	return http.StatusOK, string(b)
+}
+
+func ChannelShowHandler(db *sql.DB, client memcache.ClientShard, params martini.Params) (int, string) {
+	id := params["id"]
+	start, _ := strconv.Atoi(params["start"])
+
+	key := fmt.Sprintf("Api2/ChannelShowHandler/%s/%d", id, start)
+
+	res := client.Get(key)
+	if res.Status() == 0 {
+		return http.StatusOK, string(res.Value())
+	}
+
+	shows := GetChannelShow(db, id, start)
+	result := &Shows{shows}
+	b, err := json.Marshal(result)
+	if err != nil {
+		log.Println(err)
+		return http.StatusInternalServerError, "Oops!!!"
+	}
+
+	setCached(client, key, b)
+	return http.StatusOK, string(b)
+}
+
+func RadioListHandler(db *sql.DB, client memcache.ClientShard, params martini.Params) (int, string) {
+	key := "Api2/RadioListHandler"
+
+	res := client.Get(key)
+	if res.Status() == 0 {
+		return http.StatusOK, string(res.Value())
+	}
+
+	radios := GetRadio(db)
+	result := &Radios{radios}
+	b, err := json.Marshal(result)
+	if err != nil {
+		log.Println(err)
+		return http.StatusInternalServerError, "Oops!!!"
+	}
+
+	setCached(client, key, b)
+	return http.StatusOK, string(b)
+}
+
+func EpisodeListHandler(db *sql.DB, client memcache.ClientShard, params martini.Params) (int, string) {
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		return http.StatusInternalServerError, "Oops!!!"
+	}
+	start, _ := strconv.Atoi(params["start"])
+
+	key := fmt.Sprintf("Api2/EpisodeList/%d/%d", id, start)
+
+	res := client.Get(key)
+	if res.Status() == 0 {
+		return http.StatusOK, string(res.Value())
+	}
+	showInfo := GetShowInfo(db, id)
+	episodes := GetEpisode(db, id, start)
+	result := &Episodes{200, showInfo, episodes}
+
+	b, err := json.Marshal(result)
+	if err != nil {
+		log.Println(err)
+		return http.StatusInternalServerError, "Oops!!!"
+	}
+	setCached(client, key, b)
+	return http.StatusOK, string(b)
+}
