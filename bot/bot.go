ff9c088c14ff37c0a10882ff7566563f549c129f
Nattapong Tonprasert <luciferultram@gmail.com>
2014-10-22 04:11:29 +0700

    insert bot videos

--- bot/bot.go
+++ bot/bot.go
@@ -27,20 +27,23 @@ func NewBot(db *sql.DB) *Bot {
 }
 
 func (b *Bot) CheckYoutubeUser() {
-	youtubeUsers := getYoutubeRobotUsers(b.Db)
+	youtubeUsers := b.getYoutubeRobotUsers()
 	y := &Youtube{}
 	for _, youtubeUser := range youtubeUsers {
 		log.Println(youtubeUser.Username)
 		youtubeVideos := y.getVideoByUser(youtubeUser.Username)
 		for _, video := range youtubeVideos {
 			log.Println(video.Username, video.Title, video.VideoId)
+			if b.checkBotVideoExisting(video) {
+				b.insertBotVideo(video)
+			}
 		}
 	}
 }
 
-func getYoutubeRobotUsers(db *sql.DB) []*YoutubeUser {
+func (b *Bot) getYoutubeRobotUsers() []*YoutubeUser {
 	var youtubeUsers = []*YoutubeUser{}
-	rows, err := db.Query("SELECT username, description, user_type FROM tv_youtube_users WHERE bot = 1 ORDER BY username LIMIT 0,5")
+	rows, err := b.Db.Query("SELECT username, description, user_type FROM tv_youtube_users WHERE bot = 1 ORDER BY username LIMIT 0,5")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -62,4 +65,15 @@ func getYoutubeRobotUsers(db *sql.DB) []*YoutubeUser {
 		log.Fatal(err)
 	}
 	return youtubeUsers
+}
+
+func (b *Bot) checkBotVideoExisting(video *YoutubeVideo) bool {
+	return true
+}
+
+func (b *Bot) insertBotVideo(video *YoutubeVideo) {
+	_, err := b.Db.Exec("INSERT INTO tv_bot_videos (username, title, video_id, video_type, is_updated) VALUES (?, ?, ?, 'youtube', 0)", video.Username, video.Title, video.VideoId)
+	if err != nil {
+		panic(err)
+	}
 }
