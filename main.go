d29382a027cf110ae4d407396b526525aba15afc
Nattapong Tonprasert <luciferultram@gmail.com>
2015-07-28 16:03:46 +0700

    refactor main

--- main.go
+++ main.go
@@ -18,113 +18,141 @@ import (
 	"github.com/martini-contrib/render"
 )
 
+type CmdParam struct {
+	Command string
+	User    string
+	Channel string
+	Q       string
+	Start   int
+	Stop    int
+}
+
 func main() {
 	port := flag.String("port", "9000", "PORT")
 	command := flag.String("command", "", "COMMAND = botrun | findchannel | findvideochannel(user, channel)")
 	user := flag.String("user", "", "USER")
 	channel := flag.String("channel", "", "CHANNEL")
+	q := flag.String("q", "", "QUERY")
 	start := flag.Int("start", 0, "START")
 	stop := flag.Int("stop", 0, "STOP")
-	q := flag.String("q", "", "QUERY")
 	flag.Parse()
+
+	if *command != "" {
+		cmdParam := &CmdParam{
+			*command,
+			*user,
+			*channel,
+			*q,
+			*start,
+			*stop,
+		}
+		processCommand(cmdParam)
+		return
+	}
 
 	db, err := utils.OpenDB()
 	if err != nil {
 		panic(err.Error())
 	}
 	defer db.Close()
+	conn, err := net.Dial("tcp", os.Getenv("MEMCACHED_HOST"))
+	if err != nil {
+		panic(err.Error())
+	}
+	client := memcache.NewRawClient(0, conn)
 
-	fmt.Println(*command)
-	if *command == "krobkruakao" {
+	m := martini.Classic()
+	m.Map(db)
+	m.Map(client)
+	m.Use(render.Renderer(render.Options{
+		Directory:  "templates",
+		Layout:     "layout",
+		Delims:     render.Delims{"{[{", "}]}"},
+		Charset:    "UTF-8",
+		IndentJSON: true,
+	}))
+
+	m.Get("/", func(r render.Render) {
+		r.JSON(200, map[string]interface{}{"hello": "world สวัสดี"})
+	})
+
+	m.Group("/api2", func(r martini.Router) {
+		r.Get("/advertise", api2.AdvertiseListHandler)
+		r.Get("/section", api2.SectionListHandler)
+		r.Get("/category", api2.CategoryListHandler)
+		r.Get("/category/:id", api2.CategoryShowHandler)
+		r.Get("/category/:id/:start", api2.CategoryShowHandler)
+		r.Get("/channel", api2.ChannelListHandler)
+		r.Get("/channel/:id", api2.ChannelShowHandler)
+		r.Get("/channel/:id/:start", api2.ChannelShowHandler)
+		r.Get("/radio", api2.RadioListHandler)
+		r.Get("/episode/:id", api2.EpisodeListHandler)
+		r.Get("/episode/:id/:start", api2.EpisodeListHandler)
+	})
+
+	m.Group("/admin", func(r martini.Router) {
+		r.Get("/encrypt", admin.EncryptHandler)
+		r.Post("/encrypt", admin.EncryptUpdateHandler)
+		r.Get("/otv", admin.OtvHandler)
+		r.Post("/otv", admin.OtvProcessHandler)
+		r.Get("/botvideo", admin.BotVideoHandler)
+		r.Post("/botvideo", admin.BotVideoPostHandler)
+		r.Get("/botvideo.json", admin.BotVideoJSONHandler)
+		r.Get("/show.json", admin.ShowJSONHandler)
+		r.Get("/youtube", admin.YoutubeHandler)
+		r.Get("/youtube.search.channel", admin.YoutubeSearchChannelJSONHandler)
+		r.Get("/youtube.playlistItems", admin.YoutubePlaylistItemJSONHandler)
+		r.Get("/showlist", admin.ShowListHandler)
+		r.Get("/krobkruakao", admin.KrobkruakaoHandler)
+		r.Get("/krobkruakao.json", admin.KrobkruakaoJSONHandler)
+
+	})
+
+	m.Get("/flush", func() string {
+		client.Flush(1)
+		return "Flush!!!"
+	})
+
+	if err := http.ListenAndServe(":"+*port, m); err != nil {
+		panic(err)
+	}
+}
+
+func processCommand(cmd *CmdParam) {
+	db, err := utils.OpenDB()
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	fmt.Println(cmd.Command)
+	switch cmd.Command {
+	case "krobkruakao":
 		admin.ExampleKrobkruakao()
-	} else if *command == "botrun" {
+	case "botrun":
 		b := bot.NewBot(db)
 		b.CheckRobotChannel()
-	} else if *command == "botkrobkruakao" {
+	case "botkrobkruakao":
 		b := bot.NewBot(db)
-		b.CheckKrobkruakao(*start)
-	} else if *command == "findchannel" {
+		b.CheckKrobkruakao(cmd.Start)
+	case "findchannel":
 		b := bot.NewBot(db)
-		if *user == "" {
-			b.FindChannel()
-		}
-	} else if *command == "findvideochannel" {
+		b.FindChannel()
+	case "findvideochannel":
 		b := bot.NewBot(db)
-		if *user == "" || *channel == "" {
+		if cmd.User == "" || cmd.Channel == "" {
 			fmt.Println("Must have -user=... -channel=...")
 		} else {
-			b.CheckVideoInChannel(*user, *channel, *q)
+			b.CheckVideoInChannel(cmd.User, cmd.Channel, cmd.Q)
 		}
-	} else if *command == "findconan" {
-		b := bot.NewBot(db)
-		for i := *start; i < *stop; i++ {
-			ep := fmt.Sprintf("EP%%20%d", i)
-			b.CheckVideoInChannel("conanofficial", "UCmbpqlWIyoPEVUzU6iTf1OA", ep)
-		}
-	} else if *command == "otvupdate" {
+	case "otvupdate":
 		otv := &admin.Otv{Db: db}
 		otv.UpdateModified()
-	} else {
-		conn, err := net.Dial("tcp", os.Getenv("MEMCACHED_HOST"))
-		if err != nil {
-			fmt.Println(err)
-		}
-		client := memcache.NewRawClient(0, conn)
-
-		m := martini.Classic()
-		m.Map(db)
-		m.Map(client)
-		m.Use(render.Renderer(render.Options{
-			Directory:  "templates",
-			Layout:     "layout",
-			Delims:     render.Delims{"{[{", "}]}"},
-			Charset:    "UTF-8",
-			IndentJSON: true,
-		}))
-
-		m.Get("/", func(r render.Render) {
-			r.JSON(200, map[string]interface{}{"hello": "world สวัสดี"})
-		})
-
-		m.Group("/api2", func(r martini.Router) {
-			r.Get("/advertise", api2.AdvertiseListHandler)
-			r.Get("/section", api2.SectionListHandler)
-			r.Get("/category", api2.CategoryListHandler)
-			r.Get("/category/:id", api2.CategoryShowHandler)
-			r.Get("/category/:id/:start", api2.CategoryShowHandler)
-			r.Get("/channel", api2.ChannelListHandler)
-			r.Get("/channel/:id", api2.ChannelShowHandler)
-			r.Get("/channel/:id/:start", api2.ChannelShowHandler)
-			r.Get("/radio", api2.RadioListHandler)
-			r.Get("/episode/:id", api2.EpisodeListHandler)
-			r.Get("/episode/:id/:start", api2.EpisodeListHandler)
-		})
-
-		m.Group("/admin", func(r martini.Router) {
-			r.Get("/encrypt", admin.EncryptHandler)
-			r.Post("/encrypt", admin.EncryptUpdateHandler)
-			r.Get("/otv", admin.OtvHandler)
-			r.Post("/otv", admin.OtvProcessHandler)
-			r.Get("/botvideo", admin.BotVideoHandler)
-			r.Post("/botvideo", admin.BotVideoPostHandler)
-			r.Get("/botvideo.json", admin.BotVideoJSONHandler)
-			r.Get("/show.json", admin.ShowJSONHandler)
-			r.Get("/youtube", admin.YoutubeHandler)
-			r.Get("/youtube.search.channel", admin.YoutubeSearchChannelJSONHandler)
-			r.Get("/youtube.playlistItems", admin.YoutubePlaylistItemJSONHandler)
-			r.Get("/showlist", admin.ShowListHandler)
-			r.Get("/krobkruakao", admin.KrobkruakaoHandler)
-			r.Get("/krobkruakao.json", admin.KrobkruakaoJSONHandler)
-
-		})
-
-		m.Get("/flush", func() string {
-			client.Flush(1)
-			return "Flush!!!"
-		})
-
-		if err := http.ListenAndServe(":"+*port, m); err != nil {
-			panic(err)
+	case "findconan":
+		b := bot.NewBot(db)
+		for i := cmd.Start; i < cmd.Stop; i++ {
+			ep := fmt.Sprintf("EP%%20%d", i)
+			b.CheckVideoInChannel("conanofficial", "UCmbpqlWIyoPEVUzU6iTf1OA", ep)
 		}
 	}
 }
