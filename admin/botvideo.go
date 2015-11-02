05ca486384be36abd29e438bc3e9187aed33827d
Nattapong Tonprasert <luciferultram@gmail.com>
2015-11-02 13:53:10 +0700

    optimized getBotUsers

--- admin/botvideo.go
+++ admin/botvideo.go
@@ -81,7 +81,8 @@ func (b *BotVideo) getBotStatusID(status string) int {
 
 func (b *BotVideo) getBotUsers(selectUsername string) []*BotUser {
 	botUsers := []*BotUser{}
-	rows, err := b.Db.Query("SELECT DISTINCT v.channel_id, u.description from bot_videos v LEFT JOIN youtube_users u ON (v.channel_id = u.channel_id) WHERE u.description != '' ORDER BY description")
+	// rows, err := b.Db.Query("SELECT DISTINCT v.channel_id, u.description from bot_videos v LEFT JOIN youtube_users u ON (v.channel_id = u.channel_id) WHERE u.description != '' ORDER BY description")
+	rows, err := b.Db.Query("SELECT channel_id, description from youtube_users WHERE description != '' AND channel_id != '' ORDER BY description")
 	if err != nil {
 		panic(err)
 	}
